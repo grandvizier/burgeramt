1829768bc69b1948ce7c2508a11087bb8b84d9e9
Jeff K <admin@thedreamingjeff.com>
2015-11-17 14:08:40 +0100

    add the month/day to the message

--- check.js
+++ check.js
@@ -5,11 +5,12 @@ var twilio = require('twilio')(auth.AccountSID, auth.AuthToken);
 
 var baseUrl = 'https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&anliegen%5B%5D=120686&dienstleister=';
 var path = '.calendar-table .row-fluid td.buchbar';
+var monthXPath = '../../parent::table/thead//th[@class="month"]';
 
 var driver = new webdriver.Builder().
-   usingServer('http://127.0.0.1:4444/wd/hub').
-   withCapabilities(webdriver.Capabilities.phantomjs()).
-   build();
+    usingServer('http://127.0.0.1:4444/wd/hub').
+    withCapabilities(webdriver.Capabilities.phantomjs()).
+    build();
 
 console.log('------');
 console.log(new Date());
@@ -24,12 +25,33 @@ driver.quit();
 
 function checkSite(siteUrl, name){
     driver.get(siteUrl);
+    var msg = name + " has ";
+    var openings = 0;
+    var days = '';
     driver.findElements(webdriver.By.css(path)).then(function(elements) {
-      var msg = name + " has " + elements.length + " openings";
-      console.log(msg);
-      if (elements.length > 0){
-          sendSMS(siteUrl, msg);
-      }
+        openings = elements.length;
+        msg += openings + " openings";
+        if (openings > 0){
+            for (var i = 0; i < openings; i++) {
+                try{
+                    elements[i].getText().then(function(d) {
+                        elements[i].findElement(webdriver.By.xpath(monthXPath)).getText().then(function(m) {
+                            days += m.substring(0,3) + ' ' + d + "\n";
+                        });
+                    });
+                    if (i > 3){
+                        break;
+                    }
+                } catch (e) {
+                    console.log('meh', e);
+                }
+            }
+        }
+    }).then(function(){
+        if (openings > 0) {
+            msg += ' on: ' + days;
+            sendSMS(siteUrl, msg);
+        }
     });
 }
 
