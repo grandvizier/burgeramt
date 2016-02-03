311c06e47daa249811f98b60899d2ee925c2b8db
Jeff K <admin@thedreamingjeff.com>
2016-02-03 23:30:26 +0100

    better error handling, and a check for the next month

--- check.js
+++ check.js
@@ -11,6 +11,7 @@ var baseUrl = 'https://service.berlin.de/terminvereinbarung/termin/tag.php?termi
 var path = '.calendar-table .row-fluid td.buchbar';
 var noAppointments = '.calendar-table .row-fluid td.nichtbuchbar';
 var monthXPath = '../../parent::table/thead//th[@class="month"]';
+var nextMonthPath = 'a[title="nächster Monat"]';
 
 var driver = new webdriver.Builder().
     usingServer('http://127.0.0.1:4444/wd/hub').
@@ -36,29 +37,40 @@ function checkSite(siteUrl, name){
     driver.findElements(webdriver.By.css(noAppointments)).then(function(full) {
         if (full.length == 0){
             driver.findElement(webdriver.By.css('body')).getText().then(function(reason) {
-                console.log(name + ' unavailable: ', reason);
-                exit();
+                if (reason.indexOf("Bitte wählen Sie ein Datum") < 0) {
+                    console.log(name + ' unavailable: ', reason.substring(0,150));
+                    exit();
+                }
             });
         }
     });
     driver.findElements(webdriver.By.css(path)).then(function(elements) {
         openings = elements.length;
         msg += openings + " openings";
+        errHandling = function (err) {
+            console.log("meh\n", err);
+            console.log("\n------\n");
+        }
         if (openings > 0){
+            console.log('Found openings for ', name, openings);
             for (var i = 0; i < openings; i++) {
-                try{
-                    elements[i].getText().then(function(d) {
-                        elements[i].findElement(webdriver.By.xpath(monthXPath)).getText().then(function(m) {
-                            days += m.substring(0,3) + ' ' + d + "\n";
-                        });
-                    });
-                    if (i > 3){
-                        break;
-                    }
-                } catch (e) {
-                    console.log('meh', e);
+                elements[i].getText().then(function(d) {
+                    elements[i].findElement(webdriver.By.xpath(monthXPath)).getText().then(function(m) {
+                        days += m.substring(0,3) + ' ' + d + "\n";
+                    }, errHandling);
+                }, errHandling);
+                if (i > 3){
+                    break;
                 }
             }
+        } else {
+            // check `next` month
+            driver.findElements(webdriver.By.css(nextMonthPath)).then(function(links) {
+                links[links.length - 1].click();
+            }, errHandling);
+            driver.findElements(webdriver.By.css(path)).then(function(newElements) {
+                openings = elements.length;
+            }, errHandling)
         }
     }).then(function(){
         if (openings > 0) {
